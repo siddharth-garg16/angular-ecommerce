9a5c946421ba72c4972e790877d6dc6f0ab35cfa
Siddharth Garg <siddharthgarg16082000@gmail.com>
2023-04-22 18:47:30 +0530

    material and bootstrap setup

---
+++ ecommerce/src/app/material/material.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+
+const MaterialComponents : any[] = []
+
+@NgModule({
+  imports: [MaterialComponents],
+  exports:[MaterialComponents]
+})
+export class MaterialModule { }
